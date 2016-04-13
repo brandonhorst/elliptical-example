97e9241d2773c4ac538867112c8bc8d47b8e6d85
Brandon Horst <brandonhorst@gmail.com>
2016-04-13 10:18:37 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # elliptical-example
+
 Very basic, runnable example of Elliptical usage (from the README)
+
+## Installation
+
+```sh
+git clone https://github.com/laconalabs/elliptical-example.git  # clone repo
+cd elliptical-example  # go to the new directory
+npm install  # install dependencies
+```
+
+## Usage
+
+Elliptical grammars can be specified using Babel. Because of this, running
+the example requires `babel-node` or a build step with `babel`. This is
+provided using npm scripts. Run either
+
+```sh
+npm run example
+```
+
+or
+
+```
+npm run build
+node .
+```
---
+++ src/index.jsx
@@ -0,0 +1,91 @@
+/** @jsx createElement */
+import {createElement, compile} from 'elliptical'
+import {inspect} from 'util'
+
+// Some data to work with
+const countryData = [
+  {text: "China (People's Republic of)", value: 'CN'},
+  {text: 'Ireland', value: 'IE'},
+  {text: 'Macedonia (the former Yugoslav Republic of)', value: 'MK'},
+  {text: 'United Kingdom of Great Britain and Northern Ireland', value: 'IE'},
+  {text: 'United States', value: 'US'}
+]
+
+// Define a Phrase
+const Country = {
+  describe () {
+    return (
+      <label text='Country'>
+        <list items={countryData} strategy='fuzzy' />
+      </label>
+    )
+  }
+}
+
+// Build our grammar out of Elements
+const grammar = (
+  <sequence>
+    <literal text='flights ' />
+    <choice id='direction'>
+      <literal text='from ' value='from' />
+      <literal text='to ' value='to' />
+    </choice>
+    <Country id='country' />
+  </sequence>
+)
+
+// Obtain a parse function from our grammar
+console.log('Compiling!')
+const parse = compile(grammar)
+
+// Parse based upon a given query
+console.log('Parsing!')
+const outputs = parse('flights to irela')
+console.log(inspect(outputs, {depth: 10}))
+
+/*
+  [{ // direct match
+    words: [
+      {text: 'flights', input: true},
+      {text: ' to ', input: true},
+      {text: 'Irela', input: true, argument: 'Country'},
+      {text: 'nd', input: false, argument: 'Country'}
+    ]},
+    results: {
+      direction: 'to',
+      country: 'IE'
+    },
+    score: 1
+  }, { // mid-string match
+    words: [
+      {text: 'flights', input: true},
+      {text: ' to ', input: true},
+      {text: 'United Kingdom of Great Britain and Northern ', input: false, argument: 'Country'},
+      {text: 'Ireland', input: true, argument: 'Country'}
+    ]},
+    results: {
+      direction: 'to',
+      country: 'GB'
+    },
+    score: 0.5673076923076923
+  }, { // fuzzy match
+    words: [
+      {text: 'flights', input: true},
+      {text: ' to ', input: true},
+      {text: 'Macedon', input: false, argument: 'Country'},
+      {text: 'i', input: true, argument: 'Country'},
+      {text: 'a (the fo', input: false, argument: 'Country'},
+      {text: 'r', input: true, argument: 'Country'},
+      {text: 'm', input: false, argument: 'Country'},
+      {text: 'e', input: true, argument: 'Country'},
+      {text: 'r Yugos', input: false, argument: 'Country'},
+      {text: 'la', input: true, argument: 'Country'},
+      {text: 'v Republic of)', input: false, argument: 'Country'}
+    ]},
+    results: {
+      direction: 'to',
+      country: 'MK'
+    },
+    score: 0.024999999999999998
+  }]
+*/
